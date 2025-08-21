8f29f8e4f307945797b279f48dbf13b48c69b1ee
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-21 08:37:13 +0200

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,83 +1,114 @@
 import React from "react";
-import { useCart } from "../context/CartContext";
-import ShippingOptions from './ShippingOptions';
-import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { useCart } from "../context/CartContext"; // Custom hook to access cart state & actions
+import ShippingOptions from "./ShippingOptions"; // selecting shipping method
+import { Link } from "react-router-dom"; // Navigation 
+import { useSelector } from "react-redux"; // Access Redux store state
 
 const Cart = () => {
-    const { cart, totalCost, removeFromCart } = useCart();
-    const { selectedShipping } = useSelector((state) => state);
- 
+  // Get cart items, total cost, and remove function from CartContext
+  const { cart, totalCost, removeFromCart } = useCart();
+
+  // Get the currently selected shipping option from Redux state
+  const { selectedShipping } = useSelector((state) => state);
+
   return (
     <div className="cartPage">
       <h2>Cart</h2>
+
+      {/* If cart has items, display them. Otherwise, show empty cart message */}
       {cart.length > 0 ? (
         <>
           <ul>
             {cart.map((item, index) => (
-              <li key={index} style={{margin: '1rem'}}>
+              <li key={index} style={{ margin: "1rem" }}>
+                {/* Product image */}
                 <div style={{ flexShrink: 0 }}>
-                  <img 
-                    src={item.image} 
+                  <img
+                    src={item.image}
                     alt={item.name}
                     style={{
-                      width: '80px',
-                      height: '80px',
-                      objectFit: 'cover',
-                      borderRadius: '4px',
-                      border: '1px solid #ccc'
+                      width: "80px",
+                      height: "80px",
+                      objectFit: "cover",
+                      borderRadius: "4px",
+                      border: "1px solid #ccc",
                     }}
                   />
                 </div>
-                
-                <div style={{ 
-                  flex: 1, 
-                  display: 'flex', 
-                  flexDirection: 'column',
-                  gap: '0.25rem'
-                }}>
-                  <h5 style={{ 
-                    margin: 0, 
-                    fontSize: '1.1rem',
-                    fontWeight: '600',
-                    color: '#333'
-                  }}>
+
+                {/* Product details (name + price) */}
+                <div
+                  style={{
+                    flex: 1,
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "0.25rem",
+                  }}
+                >
+                  <h5
+                    style={{
+                      margin: 0,
+                      fontSize: "1.1rem",
+                      fontWeight: "600",
+                      color: "#333",
+                    }}
+                  >
                     {item.name}
                   </h5>
-                  <p style={{ 
-                    margin: 0, 
-                    fontSize: '1.2rem',
-                    fontWeight: 'bold',
-                    color: '#007bff'
-                  }}>
+                  <p
+                    style={{
+                      margin: 0,
+                      fontSize: "1.2rem",
+                      fontWeight: "bold",
+                      color: "#007bff",
+                    }}
+                  >
                     R{item.price}
                   </p>
                 </div>
-                </div>               
-                <div style={{width: '100px'}}>
-                <button className="btn btn-danger" onClick={() => removeFromCart(item.id)} style={{ marginLeft: "15px" }}>
-                  Remove
-                </button>
+
+                {/* Remove button */}
+                <div style={{ width: "100px" }}>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => removeFromCart(item.id)}
+                    style={{ marginLeft: "15px" }}
+                  >
+                    Remove
+                  </button>
                 </div>
               </li>
             ))}
           </ul>
+
+          {/* Display total cost of all items */}
           <h6>Total Cost: R{totalCost}</h6>
         </>
       ) : (
         <p>Your cart is empty</p>
       )}
-      {/* shipping information */}
-      <div className="shipping" style={{marginLeft: '2rem'}}>
+
+      {/* Shipping section */}
+      <div className="shipping" style={{ marginLeft: "2rem" }}>
+        {/* Show selected shipping details if chosen */}
         {selectedShipping && (
-        <p>Shipping: {selectedShipping.name}  R{selectedShipping.cost}</p>
-      )}
-       {/* Shipping component */}
-       <ShippingOptions />
-       
-       {/* help button it sends you to help section */}
-        <Link to={'/help'} style={{marginTop: "1rem"}} className='btn btn-outline-secondary'>Help ?</Link>
-        </div>
+          <p>
+            Shipping: {selectedShipping.name} R{selectedShipping.cost}
+          </p>
+        )}
+
+        {/* Allow user to pick/change shipping */}
+        <ShippingOptions />
+
+        {/* Help button links to Help page */}
+        <Link
+          to={"/help"}
+          style={{ marginTop: "1rem" }}
+          className="btn btn-outline-secondary"
+        >
+          Help ?
+        </Link>
+      </div>
     </div>
   );
 };
