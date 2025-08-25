75e9e3b4a43d145190ed1172d048cb56b087736a
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-25 18:00:33 +0200

    Update ShippingOptions.jsx
    added comments for clarity

--- src/components/ShippingOptions.jsx
+++ src/components/ShippingOptions.jsx
@@ -4,8 +4,9 @@ import { setShippingMethod } from '../redux/actions';
 
 const ShippingOptions = () => {
   const dispatch = useDispatch();
-  const { shippingOptions, selectedShipping } = useSelector((state) => state);
+  const { shippingOptions, selectedShipping } = useSelector((state) => state); // Get shipping state from Redux
 
+  // handle selection of a shipping method
   const handleSelect = (option) => {
     dispatch(setShippingMethod(option));
   };
@@ -13,14 +14,15 @@ const ShippingOptions = () => {
   return (
     <div>
       <h5 style={{color: 'grey'}}>Select Shipping Method</h5>
+      {/* Map over available shipping options */}
       {shippingOptions.map((option) => (
         <div key={option.id}>
           <input
             type="radio"
             name="shipping"
-            checked={selectedShipping?.id === option.id}
-            onChange={() => handleSelect(option)}
-            className='m-2'
+            checked={selectedShipping?.id === option.id} // Check if this option is selected
+            onChange={() => handleSelect(option)} // Update selected option in Redux
+            className='m-2' 
           />
           {option.name}  R{option.cost}
         </div>
