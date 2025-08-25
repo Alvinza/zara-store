dd9efe58f746ed09f38d9c7ecee6b217cefebd8a
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-25 16:43:03 +0200

    Update Women.jsx
    added comments for clarity

--- src/components/Women.jsx
+++ src/components/Women.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { useCart } from "../context/CartContext";
+import { useCart } from "../context/CartContext"; // Custom hook to manage cart actions
 import { Card } from "react-bootstrap";
-import womenGallery from "../assets/womenData/womenData";
+import womenGallery from "../assets/womenData/womenData"; // Array of women products
 
 function Women() {
-  const { addToCart } = useCart();
+  const { addToCart } = useCart(); // Destructure addToCart function from context
   return (
     // Store section, I used bootstrap for the card, I also used map method to render the products
     <div className="storeImage productsBackground">
       <h1 className="text-center">Women</h1>
       <div className="d-flex flex-wrap products">
+         {/* Map over womenGallery array and render each product */}
         {womenGallery.map((product) => (
           <Card key={product.id} style={{ width: "18rem", margin: "10px" }}>
             <Card.Img variant="top" src={product.image} />
